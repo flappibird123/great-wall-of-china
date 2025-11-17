4fab3adcb34cb05a2a22574b0ca67b65f5bc9190
flappibird123 <jasonlilb2@gmail.com>
2025-11-17 04:05:51 +0000

    create react project

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return(
+        <footer>
+            &copy; 2025 Max Medina, Jason Li, Jax Reid, Jarryd Chong
+        </footer>
+    );
+
+}
+
+export default Footer;
